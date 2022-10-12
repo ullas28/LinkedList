b5878e3528d9ea17a90383e065aad21662d9444b
unknown <you@example.com>
2022-10-12 16:37:24 +0530

    Added : insertfirst linked list

--- com/bridgelabz/LinkedList.java
+++ com/bridgelabz/LinkedList.java
@@ -1,77 +1,71 @@
-/*Lets create a simple Linked List of 56, 30 and 70*/
+/*Ability to create Linked List by adding 30 and 56 to 70*/
 
 package com.bridgelabz;
 
 public class LinkedList {
 
 
-        Node head;
-        static class Node {
+    static Node head;
+    static class Node {
 
-            int data;
-            Node next;
+        int data;
+        Node next;
 
-            Node(int d)
-            {
-                data = d;
-                next = null;
-            }
-        }
-
-
-        public static LinkedList append(LinkedList list, int data)
+        Node(int d)
         {
-
-            Node new_node = new Node(data);
-
-
-            if (list.head == null) {
-                list.head = new_node;
-            }
-            else {
-
-                Node last = list.head;
-                while (last.next != null) {
-                    last = last.next;
-                }
-
-
-                last.next = new_node;
-            }
-
-            return list;
-        }
-
-
-        public static void printList(LinkedList list)
-        {
-            Node currNode = list.head;
-
-            System.out.print("LinkedList: ");
-
-            while (currNode != null) {
-
-                System.out.print(currNode.data + " ");
-
-
-                currNode = currNode.next;
-            }
-        }
-
-
-        public static void main(String[] args)
-        {
-
-            LinkedList list = new LinkedList();
-
-
-            list = append(list, 56);
-            list = append(list, 30);
-            list = append(list, 70);
-
-
-            printList(list);
+            data = d;
+            next = null;
         }
     }
+
+
+    public static LinkedList append(LinkedList list, int data)
+    {
+
+        Node new_node = new Node(data);
+
+
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+
+            new_node.next = head;
+            head = new_node;
+        }
+
+        return list;
+    }
+
+
+    public static void printList(LinkedList list)
+    {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        while (currNode != null) {
+
+            System.out.print(currNode.data + " ");
+
+            currNode = currNode.next;
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        LinkedList list = new LinkedList();
+
+
+        list = append(list, 70);
+        list = append(list, 30);
+        list = append(list, 56);
+
+        printList(list);
+    }
+}
+
 
 
